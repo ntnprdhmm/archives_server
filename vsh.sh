5e32478b64266c77aabe1dd98096ffad8af13765
prudywsh <antoineprudhomme5@gmail.com>
2018-05-21 15:56:44 +0200

    started to write the vsh command: script with usage and errors functions + a condition to handle help and list options

---
+++ vsh.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+usage(){ 
+	echo "Usage: ./vsh.sh [OPTION] [PARAMETER...]"
+	echo "" 
+	echo "-h, --help			display help" 
+	echo "-list [host] [port]		list all the archives on the server"
+	echo ""
+	echo "Report bugs to antoine.prudhomme@utt.fr"
+}
+
+unrecognized_option(){
+	# $1 is the name of the optionunrecognized
+	echo "vsh: unrecognized option '$1'" >&2
+	echo "Try 'vsh --help' for more information" >&2
+}
+
+option_error(){
+	# $1 is the name of this option (-list for example)
+	echo "$1: missing operand" >&2
+	echo "Try 'vsh --help' for more information." >&2
+	exit 1
+}
+
+# No option provided 
+[[ $# -lt 1 ]] && option_error "vsh"
+
+# Handle options
+if [[ $1 == "--help" || $1 == "-h" ]];
+then 
+	usage
+elif [[ $1 == "-list" ]];
+then
+	# this option require 2 more parameters
+	[[ $# -lt 3 ]] && option_error "vsh -list"
+
+	echo "ok"	
+else
+	# the provided option is unknown
+	unrecognized_option $1
+fi
