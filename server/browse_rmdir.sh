79a1e2b551b46f29724bdbdfdc097ceb7920a0a6
prudywsh <antoineprudhomme5@gmail.com>
2018-05-27 10:26:20 +0200

    rmdir command is now deleting the children directories too

--- server/browse_rmdir.sh
+++ server/browse_rmdir.sh
@@ -2,31 +2,13 @@ archive_name=$1
 archive_path="server/archives/$archive_name"
 directory=$2
 
-# find the directory to remove
-dir_line=$(grep -P -n -m 1 "^directory\s${directory}" $archive_path | sed 's/\([0-9]*\).*/\1/')
-current_line=$dir_line
-if [[ directory ]];
-then
-	# go through each direct child
-	bash server/browse_ls.sh $archive_name $directory > temp.txt
-	while read line
-	do
-		((current_line++))
-		if [[ $(echo $line | grep -P "^([^\s]+)(\s)d.+$") ]];
-		then
-			# if it's a directory
-			sed -i "$(($current_line))s/.*//" $archive_path
-		elif [[ $(echo $line | grep -P "^([^\s]+)(\s)-.+$") ]];
-		then
-			# if it's a file
-			bash server/browse_rm.sh $archive_name "$directory/$(echo $line | cut -d " " -f 1)"
-		elif [[ $line == "@" ]];
-		then
-			break
-		fi
-	done < temp.txt
-	# clear the directory line
-	sed -i "$(($dir_line))s/.*//" $archive_path
-else
-	echo "-1"
-fi
+# remove each directory matching the given directory 
+# (== the directory concerned + his children)
+
+grep -P "^directory\s$directory(\/.+)?$" $archive_path > temp3.txt
+
+while read subdir_desc
+do
+	subdir=$(echo $subdir_desc | cut -d " " -f 2)
+	bash server/remove_dir.sh $archive_name $subdir
+done < temp3.txt
