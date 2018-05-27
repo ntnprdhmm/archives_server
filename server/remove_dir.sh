63af2e40981ce503c450f11a5df3bd05ff3d2ae8
prudywsh <antoineprudhomme5@gmail.com>
2018-05-27 11:11:17 +0200

    when rmdir, remove the directory from his parent too

--- server/remove_dir.sh
+++ server/remove_dir.sh
@@ -27,6 +27,24 @@ then
 	done < temp.txt
 	# clear the directory line
 	sed -i "$(($dir_line))s/.*//" $archive_path
+	
+	# remove the directory from his parent
+	parent=$(dirname $directory)
+	parent_line=$(grep -P -n -m 1 "^directory\s${parent}" $archive_path | sed 's/\([0-9]*\).*/\1/')
+	if [[ $parent_line ]];
+	then
+		# the deleted folder has a parent is the archive
+		# let's find the index line of the removed dir in his parent's children
+		bash server/browse_ls.sh $archive_name $parent > temp4.txt
+		line_in_parent=$(grep -P -n -m 1 "^$(basename $directory)\sd" temp4.txt | sed 's/\([0-9]*\).*/\1/')
+		if [[ $line_in_parent ]];
+		then
+			echo "parent: $(dirname $directory)"
+			echo "dir to remove: $(basename $directory)"
+			echo $((parent_line+line_in_parent))
+			sed -i "$((parent_line+line_in_parent))s/.*//" $archive_path
+		fi	
+	fi
 else
 	echo "-1"
 fi
