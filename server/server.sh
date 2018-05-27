b8888ef04d73330cc051df6b9ec17b0df4531000
prudywsh <antoineprudhomme5@gmail.com>
2018-05-27 12:51:58 +0200

    server.sh clean

--- server/server.sh
+++ server/server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+port=$1
+
 # if the backpipe doesn't exists, create it
 if [ ! -e server/backpipe ];
 then
@@ -11,38 +13,30 @@ do
 	# listen while there is a request
 	echo "server is listening"
 	# intercept the request
-	nc -lp $1 > server_in.txt
+	nc -lp $port > server_in.txt
 
 	# read from file, and split (by space) parts in array
 	cmd=$(<server_in.txt)
 	cmd_parts=( $cmd )
 
-	# do something depending on the request
-	if [[ ${cmd_parts[0]} == "list" ]];
-	then
-  		nc -l 1234 < server/backpipe | bash server/list.sh> server/backpipe
-	elif [[ ${cmd_parts[0]} == "extract" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/extract.sh ${cmd_parts[1]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "browse" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/browse.sh ${cmd_parts[1]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "browsels" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/browse_ls.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "browsecat" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/browse_cat.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "browserm" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/browse_rm.sh ${cmd_parts[1]} ${cmd_parts[2]} ${cmd_parts[3]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "browsermdir" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/browse_rmdir.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe
-	elif [[ ${cmd_parts[0]} == "direxists" ]];
-	then
-		nc -l 1234 < server/backpipe | bash server/dir_exists.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe
-	else
-		echo "Unknow command. Try 'list', 'extract' or 'browse'"
-	fi
+	case ${cmd_parts[0]} in
+		"list") 
+			nc -l $port < server/backpipe | bash server/list.sh> server/backpipe;;
+		"extract") 
+			nc -l $port < server/backpipe | bash server/extract.sh ${cmd_parts[1]} > server/backpipe;;
+		"browse")
+			nc -l $port < server/backpipe | bash server/browse.sh ${cmd_parts[1]} > server/backpipe;;
+		"browsels")
+			nc -l $port < server/backpipe | bash server/browse_ls.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe;;
+		"browsecat")
+			nc -l $port < server/backpipe | bash server/browse_cat.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe;;
+		"browserm")
+			nc -l $port < server/backpipe | bash server/browse_rm.sh ${cmd_parts[1]} ${cmd_parts[2]} ${cmd_parts[3]} > server/backpipe;;
+		"browsermdir")
+			nc -l $port < server/backpipe | bash server/browse_rmdir.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe;;
+		"direxists")
+			nc -l $port < server/backpipe | bash server/dir_exists.sh ${cmd_parts[1]} ${cmd_parts[2]} > server/backpipe;;
+		*)
+			echo "Unknow command. Try 'list', 'extract' or 'browse'";;
+	esac
 done
